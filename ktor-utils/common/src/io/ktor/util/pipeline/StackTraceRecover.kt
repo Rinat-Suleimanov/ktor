986fec11d24f9b832b584ff6d8e7f053a32032e9
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-08-16 13:07:22 +0300

    fixup! Fix pipeline stack trace

--- ktor-utils/common/src/io/ktor/util/pipeline/StackTraceRecover.kt
+++ ktor-utils/common/src/io/ktor/util/pipeline/StackTraceRecover.kt
@@ -7,8 +7,9 @@ package io.ktor.util.pipeline
 import kotlin.coroutines.*
 import kotlinx.coroutines.internal.*
 
-
 internal fun recoverStackTraceBridge(exception: Throwable, continuation: Continuation<*>): Throwable {
     @Suppress("INVISIBLE_MEMBER")
-    return recoverStackTrace(exception, continuation)
+    return recoverStackTrace(exception, continuation).withCause(exception.cause)
 }
+
+internal expect fun Throwable.withCause(cause: Throwable?): Throwable
